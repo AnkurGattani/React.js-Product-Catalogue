51cf0393a8c79adcfec6dfbe275cd6a7bb0ebb3f
Ankur Gattani <ankurgattani02@gmail.com>
2024-11-19 23:18:48 +0530

    add react-router-dom for routing and set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function App() {
-  
+
   return (
-    <>
-      
-    </>
+    <div className='min-h-screen flex flex-wrap'>
+      <header className='w-full h-[10vh] px-5 py-2 duration-300 fixed z-900 border-b-2 border-black backdrop-blur-md bg-white/30'>
+        <div className='w-full h-full mx-auto relative flex justify-between items-center py-3 '>
+          <div className='h-full p-0 flex items-center'>
+            <Link to="/" >
+              <h1 className='text-2xl font-bold'>Product Catalog</h1>
+            </Link>
+          </div>
+        </div>
+      </header>
+
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import ProductListPage from './pages/ProductListPage.jsx'
+import ProductDetailsPage from './pages/ProductDetailsPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <ProductListPage />
+      },
+      {
+        path: '/products/:id',
+        element: <ProductDetailsPage />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
