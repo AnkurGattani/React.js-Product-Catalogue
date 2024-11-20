e897e1fb4181b35ca27a16859e51356f2a127b32
Ankur Gattani <ankurgattani02@gmail.com>
2024-11-20 11:24:01 +0530

    add Redux store and product slice for state management

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import store from '../store/store.js'
+import { Provider } from 'react-redux'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import ProductListPage from './pages/ProductListPage.jsx'
@@ -26,6 +28,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
---
+++ store/productSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	products: [],
+	filteredProducts: [],
+	selectedCategories: [],
+	searchQuery: ""
+};
+
+const productSlice = createSlice({
+	name: "products",
+	initialState,
+	reducers: {
+		setProducts: (state, action) => {
+			state.products = action.payload;
+			state.filteredProducts = action.payload; // initially all the products are displayed
+			state.selectedCategories = [];
+			state.search = "";
+		},
+		filterByCategory: (state, action) => {
+			const category = action.payload;
+			if (state.selectedCategories.includes(category)) {	// if the category is already selected, remove it
+				state.selectedCategories = state.selectedCategories.filter((selectedCategory) => selectedCategory !== category);
+			} else { // if the category is not selected, add it
+				state.selectedCategories.push(category);
+			}
+			state.filteredProducts = state.products.filter((product) => 
+				(state.selectedCategories.length === 0 || state.selectedCategories.includes(product.category)) 
+				&& 
+				(state.searchQuery === "" || product.name.toLowerCase().includes(state.searchQuery.toLowerCase())) 
+			);	// filter the products based on selected categories and search query
+		},
+		searchByName: (state, action) => {
+			state.searchQuery = action.payload;
+			state.filteredProducts = state.products.filter((product) => 
+				(state.selectedCategories.length === 0 || state.selectedCategories.includes(product.category)) && (product.name.toLowerCase().includes(action.payload.toLowerCase()))
+			); // filter the products based on search query
+		}
+	}
+});
+
+export const { setProducts, filterByCategory, searchByName } = productSlice.actions;
+
+export default productSlice.reducer;
